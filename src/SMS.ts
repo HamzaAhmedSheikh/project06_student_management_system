bf255259446a1b7769d237a87ec9b91ac9e54d11
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-15 04:39:25 +0500

    The student management system's course and instructor area are now completed, allowing students to see all courses, register for the courses, and view all the instructor profiles.

--- src/SMS.ts
+++ src/SMS.ts
@@ -1,16 +1,23 @@
+import { Course } from "./Course.js";
+import { Instructor } from "./Instructor.js";
 import { Student } from "./Student.js";
+import { CourseInput, InstructorInput } from "./types.js";
 
 class SMS {
     name: string;
-    students = new Map<number, Student>();
     private _idsGenetor = 0;
+    students = new Map<number, Student>();
+    courses = new Map<string, Course>();
+    instructors = new Map<string, Instructor>();
+    balance = 0;
+
 
     constructor(_name: string) {
         this.name = _name;
     }
 
 
-    /********************* Student Section ***************/
+    /***********************  Student Section ***********************************************/
 
     getAllStudents(): Student[] {
         return [...this.students.values()]
@@ -20,13 +27,15 @@ class SMS {
         return this.students.get(id);
     }
 
-    enrollStudentinUMS(first_name: string, last_name: string, cnic: number, section: "Morning"|"Evening") {
+    enrollStudentinUMS(first_name: string, last_name: string, cnic: number, section: "Morning" | "Evening") {
         const newId = ++this._idsGenetor;
         var password = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
         const student = new Student(first_name, last_name, cnic, section, newId, password);
         this.students.set(newId, student);
         return { id: newId, password: password };
-      }
+    }
+
+    /************************ sign up, login in section **************************************/
 
     logIn(studentId: number, password: string): { status: boolean, msg: string } {
 
@@ -83,8 +92,70 @@ class SMS {
             throw Error("Incorrect password");
         }
 
-    }   
+    }
 
+    /************************ Course Section ************************************************/
+
+    AddCoursesinUMS(_courses: CourseInput[]) {
+        _courses.map((course) => {
+            const { course_code, course_name, description, instructor, tuition_fee } = course;
+            const newCourse = new Course(course_code, course_name, description, instructor, tuition_fee)
+            this.courses.set(course.course_code, newCourse);
+        })
+    }
+
+    enrollInCourses(studentId: number, courseCodes: string[]) {
+        let student = this.students.get(studentId);
+        if (!student) throw Error("Student do not exist");
+        if (!student.loginStatus) throw Error("You are not logged In");
+
+        let totalFeeToPay = 0
+
+        courseCodes.map((code: string) => {
+            const course = this.courses.get(code);
+            if (course) {
+                totalFeeToPay = totalFeeToPay + course.tuition_fee;
+            }
+        })
+
+        if (student.balance < Number(totalFeeToPay)) {
+            throw Error("You don't have enough balance to enroll in these courses");
+        }
+
+        courseCodes.forEach((code) => {
+            const course = this.courses.get(code)
+            if (!course) throw Error(`course do not exist ${code}`);
+
+            if (student) {
+                this.balance += course.tuition_fee;
+                student.balance -= course.tuition_fee;
+                student.courses_enrolled.push(course.course_code);
+            }
+        })
+
+        this.students.set(studentId, student);
+    }
+
+    getAllCourseInUMS(): Course[] {
+        return [...this.courses.values()]
+    }
+
+    getCourseById(code: string): Course | undefined {
+        return this.courses.get(code);
+    }
+
+    /************************* Instructor Section ******************************************/
+
+    AddInstructorsInUMS(_instructors: InstructorInput[]) {
+        _instructors.map((instructor) => {
+          let newInstructor = new Instructor(instructor.instructor_name, instructor.course_codes)
+          this.instructors.set(instructor.instructor_name, newInstructor);
+        })
+    }
+
+    getAllInstructorsInUMS(): Instructor[] {
+        return [...this.instructors.values()]
+    }
 }
 
-export default new SMS("PIAIC STUDENT MANAGEMENT SYSTEM");
+export default new SMS("STUDENT MANAGEMENT SYSTEM");
---
+++ src/types.ts
@@ -0,0 +1,76 @@
+export type CourseInput = {
+    course_code: string,
+    course_name: string,
+    description: string,
+    instructor: string,
+    tuition_fee: number
+}
+
+export type InstructorInput = {
+    course_codes: string[],
+    instructor_name: string,
+}
+
+export const coursesList = [
+    {
+        course_code: "01MD",
+        course_name: "Meraverse Development",
+        description: "Meraverse Development",
+        instructor: "Zia Khan",
+        tuition_fee: 3000
+    },
+    {
+        course_code: "02BC",
+        course_name: "Blockchain",
+        description: "Blockchian",
+        instructor: "Zeeshan Hanif",
+        tuition_fee: 5000
+    },
+    {
+        course_code: "03CC",
+        course_name: "Cloud Computing",
+        description: "Cloud Computing",
+        instructor: "Daniyal Nagori",
+        tuition_fee: 4000
+    },
+    {
+        course_code: "04AI",
+        course_name: "Artificial Intelligence",
+        description: "Artificial Intelligence",
+        instructor: "Zia Khan",
+        tuition_fee: 2500
+    },
+    {
+        course_code: "05IOT",
+        course_name: "Internet of things",
+        description: "Internet of things",
+        instructor: "Zia Khan",
+        tuition_fee: 2800
+    },
+    {
+        course_code: "CNC",
+        course_name: "Cloud Native Computing",
+        description: "Cloud Native Computing",
+        instructor: "Adil Altaf",
+        tuition_fee: 6000
+    }
+];
+
+export const instructorsList = [
+    {
+        instructor_name: "Zia Khan",
+        course_codes: ["01WMD", "04AI", "05IOT"]
+    },
+    {
+        instructor_name: "Daniyal Nagori",
+        course_codes: ["03CC"]
+    },
+    {
+        instructor_name: "Zeeshan Hanif",
+        course_codes: ["02BC"]
+    },
+    {
+        instructor_name: "Adil Altaf",
+        course_codes: ["CNC"]        
+    }
+];
